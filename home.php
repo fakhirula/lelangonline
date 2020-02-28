e5c37e4f64ff6f09f4eb010e1b3080630abf3500
fakhirula <inyour.nase@gmail.com>
2020-02-28 10:16:56 +0700

    memperbaiki beberapa rancu

--- home.php
+++ home.php
@@ -27,46 +27,6 @@
     <?php } ?>
   </div>
 </div>
-
-
-    <div class="row">
-      <div class="col-md-4 mb-5  shadow p-3 rounded">
-        <div class="card h-100">
-          <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-5">
-        <div class="card h-100">
-          <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-5">
-        <div class="card h-100 shadow p-3 rounded">
-          <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-          </div>
-          <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
-          </div>
-        </div>
-      </div>
-    </div>
     <hr>
     <!-- /.row -->
     <div class="row">
--- page/lelang/lelang.php
+++ page/lelang/lelang.php
@@ -19,14 +19,14 @@
     foreach( $querykamu as $row ) :
 ?>
       <div class="col-md-4 mb-5">
-        <div class="card h-100 border-primary shadow p-3 rounded">
+        <div class="card h-100 shadow">
           <img class="card-img-top" src="http://placehold.it/300x200" alt="">
           <div class="card-body">
             <h4 class="card-title"><?= $row['nama_barang'] ?></h4>
             <p class="card-text">Harga: Rp.<?= $row['harga_awal'] ?></p>
           </div>
           <div class="card-footer">
-            <a href="#" class="btn btn-primary">Find Out More!</a>
+            <a href="#" class="btn btn-primary">Detail!</a>
           </div>
         </div>
       </div>
